8bc40aeb75648428c1df9fe1261d9cbc84d82cae
phantomdevi1 <[gruzdev_ilya16@mail.ru]>
2024-05-02 17:32:43 +0300

    contacts

---
+++ contacts.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Контакты</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+</head>
+<body>
+<header>
+    <img src="img/up_image1.svg" alt="" height="80px">
+    <div class="toolbar">
+        <a href="index.html">Главная</a>
+        <a href="news.php">Новости</a>
+        <a href="services.php">Услуги</a>
+        <a href="contacts.php">Контакты</a>
+        <a href="reviews.php">Отзывы</a>
+    </div>
+    <img src="img/up_image2.svg" alt="" height="80px">
+</header>
+<div class="contacts_content">
+<p class="content_title">Контакты</p>
+<div class="phone_contacts-container">
+  <p>Телефон</p>
+  <a href="tel:+79201548208">+7(920)-154-82-08</a>
+</div>
+<div class="adress_contacts-container">
+  <p class="adress_contacts-title">Адрес</p>
+  <p class="adress_contacts-text">Город Тверь проспект Чайковского, 23</p>
+</div>
+<div class="socseti_contacts-container">
+  <p class="socseti_contacts-title">Соцсети</p>
+  <div class="socseti_contacts-href">
+    <a href=""><img src="img/vk.svg" alt=""></a>
+    <a href=""><img src="img/tg.svg" alt=""></a>
+  </div>
+</div>
+<form method="POST" class="callback_form">
+  <p>Остались вопросы?</p>
+  <input type="text" name="" id="" placeholder="Имя">
+  <input type="number" name="" id="" placeholder="Номер телфона">
+  <input type="text" name="" id="" placeholder="Сыылка на соц.сеть">
+</form>
+</div>
+
+</body>
+</html>
